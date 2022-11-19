ffd55cf47c2464821ecf98abc6ca33e223feee2a
DenisMedko <medko.denis76@gmail.com>
2022-11-19 21:35:20 +0200

    setTimeout method added and refactoring

--- src/componenets/watchController/index.jsx
+++ src/componenets/watchController/index.jsx
@@ -4,7 +4,15 @@ import StartStopButton from '../StartStopButton';
 import styles from './WatchController.module.css';
 import LapList from '../LapsList/LapList';
 
+const timerMethodEnum = {
+    setTimeout : 'setTimeout',
+    setInterval : 'setInterval',
+}
+const TIMER_METHOD = timerMethodEnum.setTimeout; //setInterval
+
 const TIME_TICK_INTERVAL = 1000;
+
+
 const initialState = {
     startTimerTime: 0,
     workingTimerTime: 0,
@@ -20,64 +28,62 @@ class WatchController extends Component {
         this.state = initialState;
         this.timeInterval = null;
     }
-    tick = () => {
-       this.setState((state) => ({
-            ...state,
-            //time: state.time + 1,
-            currentTime: Date.now(),
-        }));
-        //debugger;
-    }
+    
     componentDidMount() {
         this.startStop();
-        //removed strict mode in App
+        //better to remove strict mode
     }
-    componentDidUpdate(prevState, currentState) {
-        //console.log('WatchController DidUpdate');
-        //console.log(prevState);
-        //debugger;    
+
+    componentDidUpdate() {
+        if (TIMER_METHOD === 'setTimeout') {
+            this.timeInterval = this.state.isRunning 
+                && setTimeout(this.tick, TIME_TICK_INTERVAL);
+        }
     }
+    
     componentWillUnmount() {
         clearInterval(this.timeInterval);
     }
     
-    startStop = () => {
-        
+    tick = () => {
+        this.setState((state) => ({
+             ...state,
+             //time: state.time + 1,
+             currentTime: Date.now(),
+         }));
+         //debugger;
+    }
+     
+    startStop = () => { 
         if (this.state.isRunning) {
             clearInterval(this.timeInterval);    
-        } 
-        else {
-            this.timeInterval = setInterval(this.tick, TIME_TICK_INTERVAL);
+        } else {
+            this.timeInterval = TIMER_METHOD === 'setInterval' 
+                && setInterval(this.tick, TIME_TICK_INTERVAL);
         };
-
         
         //debugger;
         this.setState((state) => {
             const now = Date.now();
-            //if (!state.isRunning) {debugger}
             return {
                 ...state,
                 currentTime : state.isRunning ? state.currentTime : now ,
                 startTimerTime : state.isRunning ? state.startTimerTime : now,
                 workingTimerTime: !state.isRunning 
-                    ? state.workingTimerTime + state.currentTime - state.startTimerTime 
-                    : state.workingTimerTime,
+                     ? state.workingTimerTime + state.currentTime - state.startTimerTime 
+                     : state.workingTimerTime,
                 startLapTime : state.isRunning ? state.startLapTime : now,
                 isRunning : !state.isRunning,
             }
-        });
-         
+        });    
     }
 
     reset = () => {
         clearInterval(this.timeInterval);
         this.setState(initialState);
-
     }
 
     addLap = () => {
-        //Используем state из callback и получаем проблему, см ниже
-        //const now = this.state.isRunning ? Date.now() : this.state.currentTime;
         const now = Date.now();
         this.setState((state) => ({
             ...state,
@@ -90,30 +96,18 @@ class WatchController extends Component {
                 }
             ],
             startLapTime : now,
-            
-            //Проблема 1: Если использовать state из callback, при остановленом таймере,
-            //приходится обновлять не измененный state -> лишний рендер ???
-            //Решение ? disable на кнопку Lap на этапе рендеринга при остановке таймера
-
-            //Проблема 2. Если перенести массив laps в состояние компонента LapList,
-            // (чтоб не перерисовывать при нажатии на кнопку Lap весь компонент WatchController)
-            // то как перерендерить LapList после обнуления Reset-ом
         }));    
     }
 
     render() {
-        //const prevTotalTime = this.state.laps.length
-        //? this.state.laps[this.state.laps.length - 1].totalTime
-        //: 0;
         //debugger;
+        const {currentTime, startTimerTime, workingTimerTime, isRunning, laps} = this.state;
         return (
             <div className={styles.container}>
-                <TimeBoard time={this.state.currentTime - this.state.startTimerTime + this.state.workingTimerTime} />
+                <TimeBoard time={currentTime - startTimerTime + workingTimerTime} />
                 <div className={styles.btnContainer}>
-                    {/*<StartStopButton btnName='Start' onClick={this.start}/>*/}
-                    {/*<StartStopButton btnName='Stop' onClick={this.stop}/>*/} 
                     <StartStopButton 
-                        btnName={this.state.isRunning ? 'Stop' : 'Start'} 
+                        btnName={isRunning ? 'Stop' : 'Start'} 
                         onClick={this.startStop}
                     />
                     <StartStopButton 
@@ -123,11 +117,10 @@ class WatchController extends Component {
                     <StartStopButton 
                         btnName='Lap' 
                         onClick={this.addLap}
-                        disabled={!this.state.isRunning}
-                    /> 
-                    
+                        disabled={!isRunning}
+                    />      
                 </div>
-                <LapList laps={this.state.laps}/>   
+                <LapList laps={laps}/>   
             </div>
         );
     }
